eaba2d9af46d13aa1afc10dcc72d46ed0dd25bca
ChatlaRavi <85101364+ChatlaRavi@users.noreply.github.com>
2023-07-22 19:18:58 +0530

    Add files via upload

---
+++ Variables.tf
@@ -0,0 +1,32 @@
+
+
+variable RGname {
+  type        = string
+  default     = "testRG"
+ 
+}
+variable location {
+  type        = string
+  default     = "EastUS"  
+}
+variable App Serviceplan{
+ type        = string
+  default     = "Appplan-test" 
+}
+ Variable kind{
+ type        = string
+  default     = "Standard" 
+ }
+Variable Virtual Network{
+ type        = string
+  default     = "VNET1"  
+ }
+ Variable subnet{
+ type        = string
+  default     = "subnet1"  
+ }
+ Variable virtual machine{
+ type        = string
+  default     = "vm1"  
+ }
+ 
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+RGname = testrg
+location = Eastus
+App Serviceplan = kpmgplan
+Kind = standard
+Virtual Network = kpmg-vnet
+subnet = kpmg-subnet
+Virtual machine = kpmg-VM
